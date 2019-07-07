3dc9e24b874b023527c79a7f37cb7431cf14dd53
Alexey Ponomaruev <alexatiks@gmail.com>
2019-07-07 13:28:29 +0300

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -6,7 +6,8 @@ rm  -rf ~/Library/Preferences/IntelliJIdea*/eval
 echo "removing all evlsprt properties in options.xml..."
 sed -i  '' '/evlsprt/d' ~/Library/Preferences/IntelliJIdea*/options/other.xml
 
-echo "removing java.util.prefs.plist..."
+echo "removing some plist files..."
 rm ~/Library/Preferences/com.jetbrains.intellij.plist
+rm ~/Library/Preferences/jetbrains.idea.*.plist
 
 echo "That's it, enjoy ;)"

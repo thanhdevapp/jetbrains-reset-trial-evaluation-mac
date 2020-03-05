92c3969d08835ac2b2dcfda2c596df80c8e00c41
Alexey Ponomaruev <alexatiks@gmail.com>
2020-03-05 19:51:34 +0300

    Remove additional files only once

--- runme.sh
+++ runme.sh
@@ -8,12 +8,14 @@ for product in IntelliJIdea WebStorm DataGrip; do
 
   echo "removing all evlsprt properties in options.xml..."
   sed -i  '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
-
-  echo "removing additional plist files..."
-  rm ~/Library/Preferences/com.apple.java.util.prefs.plist
-  rm ~/Library/Preferences/com.jetbrains.*.plist
-  rm ~/Library/Preferences/jetbrains.*.*.plist
+  
   echo " "
 done
 
+echo "removing additional plist files..."
+rm ~/Library/Preferences/com.apple.java.util.prefs.plist
+rm ~/Library/Preferences/com.jetbrains.*.plist
+rm ~/Library/Preferences/jetbrains.*.*.plist
+
+echo " "
 echo "That's it, enjoy ;)"

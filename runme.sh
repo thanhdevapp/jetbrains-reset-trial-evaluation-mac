b7c806e19a2073e09a7898c8b4e4ad489ca8ed16
Mayur Padshala <mayurpatel@novuslogics.com>
2020-05-11 13:31:18 +0530

    Fix paths for 2020.1+ products (#3)

--- runme.sh
+++ runme.sh
@@ -4,11 +4,17 @@ for product in IntelliJIdea WebStorm DataGrip PhpStorm; do
   echo "Resetting trial period for $product"
 
   echo "removing evaluation key..."
-  rm  -rf ~/Library/Preferences/$product*/eval
+  rm -rf ~/Library/Preferences/$product*/eval
+  
+  // Above path not working on latest version. Fixed below
+  rm -rf ~/Library/Application\ Support/JetBrains/$product*/eval 
 
   echo "removing all evlsprt properties in options.xml..."
-  sed -i  '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
+  sed -i '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
   
+  // Above path not working on latest version. Fixed below
+  sed -i '' '/evlsprt/d' ~/Library/Application\ Support/JetBrains/$product*/options/other.xml
+
   echo
 done
 

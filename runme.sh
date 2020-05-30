ee921bb05b2ff32127a74774f119134457765432
Alexey Ponomaryov <alexatiks@gmail.com>
2020-05-30 11:29:34 +0300

    Add launchd agent for resetting evaluation in background

--- runme.sh
+++ runme.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+if [ $1 = "--prepare-env" ]; then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  mkdir -p ~/Scripts
+
+  echo "Copying the script to $HOME/Scripts"
+  cp -rf $DIR/runme.sh  ~/Scripts/jetbrains-reset.sh
+  chmod +x ~/Scripts/jetbrains-reset.sh
+
+  echo
+  echo "Copying com.jetbrains.reset.plist to $HOME/Library/LaunchAgents"
+  cp -rf $DIR/com.jetbrains.reset.plist ~/Library/LaunchAgents
+
+  echo
+  echo "Loading job into launchctl"
+  launchctl load ~/Library/LaunchAgents/com.jetbrains.reset.plist
+
+  echo
+  echo "That's it, enjoy ;)"
+  exit 0
+fi
+
+if [ $1 = "--launch-agent" ]; then
+  PROCESS=(idea webstorm datagrip phpstorm clion pycharm goland rubymine rider)
+  COMMAND_PRE=("${PROCESS[@]/#/MacOS/}")
+
+  # Kill all Intellij appcications
+  kill -9 `ps aux | egrep $(IFS=$'|'; echo "${COMMAND_PRE[*]}") | awk '{print $2}'`
+fi
+
+# Reset Intellij evaluation
 for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine Rider; do
   echo "Resetting trial period for $product"
 
@@ -25,3 +55,9 @@ rm -f ~/Library/Preferences/jetbrains.*.*.plist
 
 echo
 echo "That's it, enjoy ;)"
+
+# Flush preference cache
+if [ $1 = "--launch-agent" ]; then
+  killall cfprefsd
+  echo "Evaluation was reset at $(date)" >> ~/Scripts/logs
+fi

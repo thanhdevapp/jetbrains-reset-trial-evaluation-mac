457abab4713874d7a1b2600f24f574b9a0f6b507
thanhdevapp <thanhdevapp@gmail.com>
2021-01-09 15:10:04 +0700

    Update runme.sh
    test work on mac Webstorm and Rider

--- runme.sh
+++ runme.sh
@@ -30,17 +30,18 @@ if [ "$1" = "--launch-agent" ]; then
 fi
 
 # Reset Intellij evaluation
-for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine Rider; do
+#for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine Rider; do
+for product in WebStorm Rider; do
   echo "Resetting trial period for $product"
 
-  echo "removing evaluation key..."
-  rm -rf ~/Library/Preferences/$product*/eval
+#  echo "removing evaluation key..."
+#  rm -rf ~/Library/Preferences/$product*/eval
 
   # Above path not working on latest version. Fixed below
   rm -rf ~/Library/Application\ Support/JetBrains/$product*/eval
 
-  echo "removing all evlsprt properties in options.xml..."
-  sed -i '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
+#  echo "removing all evlsprt properties in options.xml..."
+#  sed -i '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
 
   # Above path not working on latest version. Fixed below
   sed -i '' '/evlsprt/d' ~/Library/Application\ Support/JetBrains/$product*/options/other.xml
@@ -52,6 +53,7 @@ echo "removing additional plist files..."
 rm -f ~/Library/Preferences/com.apple.java.util.prefs.plist
 rm -f ~/Library/Preferences/com.jetbrains.*.plist
 rm -f ~/Library/Preferences/jetbrains.*.*.plist
+
 
 echo
 echo "That's it, enjoy ;)"
